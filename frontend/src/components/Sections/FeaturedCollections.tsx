307d802203ae8d4597edee08b9352cc4f7bd1416
ThibautMilville <tmilville.pro@gmail.com>
2025-03-06 21:31:20 +0100

    feat(main): Featured collections section correction | filters

--- frontend/src/components/Sections/FeaturedCollections.tsx
+++ frontend/src/components/Sections/FeaturedCollections.tsx
@@ -8,19 +8,21 @@ interface FeaturedCollectionsProps {
 function FeaturedCollections({collections}: FeaturedCollectionsProps) {
   return (
     <div className='container mx-auto px-4 py-12'>
-      <div className='flex flex-col sm:flex-row justify-between items-center mb-8'>
-        <h2 className='text-2xl font-cabin font-bold text-primary-300 mb-4 sm:mb-0'>Featured Collections</h2>
+      <div className='flex justify-between items-center mb-8'>
+        <h2 className='text-2xl font-cabin font-bold text-primary-300'>Featured Collections</h2>
         <Link to='/collections' className='text-gray-400 hover:text-white font-medium'>
           View all collections →
         </Link>
       </div>
 
-      <div className='flex flex-nowrap overflow-x-auto sm:flex-wrap justify-between items-center mb-4 space-x-4 sm:space-x-6 pb-2 sm:pb-0'>
-        <button className='text-white font-medium border-b-2 border-primary-500 whitespace-nowrap'>All</button>
-        <button className='text-gray-400 hover:text-white whitespace-nowrap'>Art</button>
-        <button className='text-gray-400 hover:text-white whitespace-nowrap'>Collectibles</button>
-        <button className='text-gray-400 hover:text-white whitespace-nowrap'>Game Assets</button>
-        <button className='text-gray-400 hover:text-white whitespace-nowrap'>Music</button>
+      <div className='flex justify-between items-center mb-4'>
+        <div className='flex space-x-6 font-cabin'>
+          <button className='text-white font-medium border-b-2 border-primary-500'>All</button>
+          <button className='text-gray-400 hover:text-white'>Art</button>
+          <button className='text-gray-400 hover:text-white'>Collectibles</button>
+          <button className='text-gray-400 hover:text-white'>Game Assets</button>
+          <button className='text-gray-400 hover:text-white'>Music</button>
+        </div>
       </div>
 
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
