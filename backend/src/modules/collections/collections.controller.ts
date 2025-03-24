b0337c8d3e3e2ba959aa9d062bf02cc249311a35
ThibautMilville <tmilville.pro@gmail.com>
2025-03-24 23:46:30 +0100

    feat(main): Add Joomla API config + test collections

--- backend/src/modules/collections/collections.controller.ts
+++ backend/src/modules/collections/collections.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Header } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { CollectionsService } from './collections.service';
 
 @Controller('collections')
@@ -6,9 +6,6 @@ export class CollectionsController {
   constructor(private readonly collectionsService: CollectionsService) {}
 
   @Get()
-  @Header('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
-  @Header('Pragma', 'no-cache')
-  @Header('Expires', '0')
   async getCollections() {
     return this.collectionsService.getCollections();
   }
