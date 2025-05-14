f76037969eaf845ca4184f247300165a8a79bfd9
ThibautMilville <tmilville.pro@gmail.com>
2025-05-14 22:23:50 +0200

    feat(main): Redirection for /launchpad-ut

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,31 @@
-import {defineConfig, loadEnv} from 'vite'
+import {defineConfig, Plugin} from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
 import path from 'path'
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, process.cwd(), '')
   const certExists = fs.existsSync('./.cert/cert.pem') && fs.existsSync('./.cert/key.pem')
 
+  const redirectPlugin: Plugin = {
+    name: 'redirect-plugin',
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        const url = req.url || ''
+
+        if (url === '/launchpad-ut') {
+          res.writeHead(301, {Location: '/launchpad-ut/'})
+          res.end()
+          return
+        }
+
+        next()
+      })
+    },
+  }
+
   return {
-    plugins: [react()],
-    base: mode === 'production' ? '/' : `/${env.VITE_APP_PATHNAME}/`,
+    plugins: [react(), redirectPlugin],
+    base: mode === 'production' ? '/' : `/launchpad-ut/`,
     server: {
       proxy: {
         '/api': {
@@ -17,6 +33,9 @@ export default defineConfig(({mode}) => {
           changeOrigin: true,
           rewrite: path => path.replace(/^\/api/, ''),
         },
+      },
+      historyApiFallback: {
+        rewrites: [{from: /^\/launchpad-ut$/, to: '/launchpad-ut/'}],
       },
       ...(certExists
         ? {
@@ -30,7 +49,7 @@ export default defineConfig(({mode}) => {
     preview: {
       host: '0.0.0.0',
       port: 8080,
-      allowedHosts: ['launchpad-2ycml.ondigitalocean.app', 'localhost']
+      allowedHosts: ['launchpad-2ycml.ondigitalocean.app', 'localhost'],
     },
     resolve: {
       alias: {
